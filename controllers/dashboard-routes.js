110e6ba6bc76241770075c9348a6024b540efeba
Xiaojing <8redfishcreative@gmail.com>
2022-11-18 20:01:29 -0500

    create login page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,3 +1,49 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+//Get all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+            where: {
+                user_id: req.session.user_id
+                },
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'added_date'
+               ],
+            include: [{
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id','added_date'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('dashboard',{ posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//Get single post
+router.get('')
+
+router.get('/new', (req, res) => {
+    res.render('new-post');
+});
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -36,4 +36,31 @@ router.get('/', (req, res) => {
         });
 });
 
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+router.get('/dashboard', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('dashboard');
+});
 module.exports = router;
