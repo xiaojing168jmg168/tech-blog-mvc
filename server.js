3e70205964c41b2b8271fd7f6007f8ce89e9a164
Xiaojing Deng <8redfishcreative@gmail.com>
2022-11-22 23:12:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const helpers = require('./utils/helpers');
 
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 const sess = {
   secret: 'Super secret secret',
